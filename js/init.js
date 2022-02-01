4ece5ffbbb057fe73ba1bd253b322ea72700f2cb
trixaew <biatsmax@gmail.com>
2022-01-31 23:31:14 -0300

    lista firebase

---
+++ js/init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3yN1Mot9kp96g6Peuu3D3mYYdK4bbEqY",
+  authDomain: "lista-tarefa-aa48b.firebaseapp.com",
+  projectId: "lista-tarefa-aa48b",
+  storageBucket: "lista-tarefa-aa48b.appspot.com",
+  messagingSenderId: "548116052682",
+  appId: "1:548116052682:web:62c6cc504c5f4386ac5233"
+};
+
+ const app= firebase.initializeApp(firebaseConfig)
+ 
